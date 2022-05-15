a891168a5dfb0ddf554d62bba75591bc2e7222fd
Triplerush <fcanal@unsa.edu.pe>
2022-05-15 10:56:29 -0500

    Solucion ejercicio 1 y 2

--- src/main/java/com/example/project/Exercise2.java
+++ src/main/java/com/example/project/Exercise2.java
@@ -1,4 +1,4 @@
-package com.example.project;
+package com.example.project;;
 
 import java.util.Scanner;
 
@@ -10,22 +10,32 @@ public class Exercise2 {
 		Scanner sc = new Scanner(System.in);
 		while (true) {
 
-			int n = sc.nextInt(), suma;
+			int n = sc.nextInt(), suma, temp;
 			int a[] = new int[n];
 
 			for (int i = 0; i < n; i++) {
-				n = sc.nextInt();
-				a[i] = n;
+				temp = sc.nextInt();
+				a[i] = temp;
 			}
-
 			suma = sc.nextInt();
 
-			System.out.println(obj.esSubconjuntoSumaExt(a, suma));
+			System.out.println(obj.esSubconjuntoSuma(a,0, suma));
 		}
 	}
+	
+	public boolean esSubconjuntoSuma(int conjunto[],int n, int suma) {
+		if (suma == 0)
+			return true;
+		if (n == conjunto.length)
+			return false;
+		
+		if (conjunto[n] % 7 == 0 && n != conjunto.length-1) {
+			if (conjunto[n + 1] == 1)
+				return esSubconjuntoSuma(conjunto, n + 1, suma);
+			else
+				return esSubconjuntoSuma(conjunto, n + 1, suma - conjunto[n]);
+		} else
+			return esSubconjuntoSuma(conjunto, n + 1, suma - conjunto[n]) || esSubconjuntoSuma(conjunto, n + 1, suma);
 
-	public boolean esSubconjuntoSumaExt(int a[], int suma) {
-
-		return false;
 	}
 }
